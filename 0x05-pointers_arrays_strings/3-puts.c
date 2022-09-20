c67241960a2923ec5a721e120c80e7bcef8ee2a8
Emmygee80 <emmanueldeleomoniyi@gmail.com>
2022-09-20 12:35:54 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,14 +7,7 @@
  *
  *Description: prints a string
  *On success: returns no error
- */
+ */								
 
-void _puts(char *str)
-{
- 	int i = 0;
-	while (*(str + i) != '\0')
-	{
-	   	putchar(*(str + i));
-	}
-	putchar(10);
-}	
+void _puts(char *str)						
+{									int i = 0;							while	(*(str + i) !=	'\0')						{								putchar(*(str + i));							i++;								}								
